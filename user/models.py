49e510ab7f4c7e9b1b7df6d7ee9b2e78a32eac52
torishabhmehta <torishabhmehta@gmail.com>
2018-09-26 00:02:37 +0530

    made a basic custom user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+import datetime
+import os
 
-# Create your models here.
+class User(Model):
+    """
+    This model contains all the basic information about an existing user
+    """
+
+    username = models.CharField(
+        max_length=63,
+        unique=True,
+    )
+
+    email = models.EmailField(
+      unique = True,
+    )
+
+    name = models.CharField(
+        max_length=63,
+    )
+
+    date = models.DateField(
+        default=datetime.date.today,
+    )
+
+    last_active = models.DateField()
+    
+    picture = models.ImageField(
+        upload_to=UploadTo(),
+        blank=True,
+        null=True,
+    )
+
+    likes = models.IntegerField()
+
+
+    def __str__(self):
+        """
+        Return a string representation of the model
+        :return: a string representation of the model
+        """
+
+        username = self.username
+        return f'{username}}'
