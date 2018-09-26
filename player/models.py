a945af345f3a42520a36f20b204e294ef62f5750
torishabhmehta <torishabhmehta@gmail.com>
2018-09-26 20:22:22 +0530

    Improved model Channels

--- player/models.py
+++ player/models.py
@@ -3,12 +3,10 @@ import datetime
 import os
 
 
-class Channels(Model):
+class AbstractChannels(models.Model):
     """
     This model contains all the basic information neded for creating a channel
     """
-
-    user = models.ForeignKey(User, blank=True,)
 
     Channel_name = models.CharField(
         max_length=63,
@@ -31,6 +29,10 @@ class Channels(Model):
         null=True,
     )
 
+
+    class Meta:
+        abstract = True
+
     def __str__(self):
         """
         Return a string representation of the model
@@ -38,7 +40,13 @@ class Channels(Model):
         """
 
         name = self.Channel_name
-
         return f'{name} '
+
+class UserChannel(AbstractChannels):
+    user = models.ForeignKey(User, unique=True)
+
+class CommonChannel(AbstractChannels):
+
+
 
 
