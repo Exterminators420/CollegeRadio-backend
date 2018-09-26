21cca63635a39af05137b7175d87eff7aaa71ced
Rohit <rohit.uph2017@iitr.ac.in>
2018-09-27 04:20:59 +0530

    test

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,3 @@
-from django.conf.urls import include
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
+
  
-]
