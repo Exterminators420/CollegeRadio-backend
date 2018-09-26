1325b3a7eeb79cdab2f910360cc53680f9244995
Rohit <rohit.uph2017@iitr.ac.in>
2018-09-27 02:54:33 +0530

    Changed upload to fxn

--- player/models.py
+++ player/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 import datetime
 import os
-
 
 def UploadTo(instance, filename):
     """
     Function for changing the upload image name
     """
-    upload_to = '/Desktop/photos'
+    upload_to = '/Desktop/photos/channels'
     ext = filename.split('.')[-1]
     filename = '{}.{}'.format(instance.pk, ext)
     return os.path.join(upload_to, filename)
