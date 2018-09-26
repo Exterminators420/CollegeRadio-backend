35c3fcb85cd07524d234b9fbe89b7b072d411879
torishabhmehta <torishabhmehta@gmail.com>
2018-09-26 20:42:35 +0530

    defined seperate private and public channels

--- player/models.py
+++ player/models.py
@@ -3,7 +3,17 @@ import datetime
 import os
 
 
-class AbstractChannels(models.Model):
+def UploadTo(instance, filename):
+    """
+    Function for changing the upload image name
+    """
+    upload_to = '/Desktop/photos'
+    ext = filename.split('.')[-1]
+    filename = '{}.{}'.format(instance.pk, ext)
+    return os.path.join(upload_to, filename)
+
+
+class AbstractChannel(models.Model):
     """
     This model contains all the basic information neded for creating a channel
     """
@@ -21,16 +31,22 @@ class AbstractChannels(models.Model):
         default=datetime.date.today,
     )
 
-    likes = models.IntegerField(default=0)
+    likes = models.IntegerField(
+        default=0
+    )
     
     picture = models.ImageField(
-        upload_to=UploadTo(),   #tbd
+        upload_to=UploadTo,
         blank=True,
         null=True,
     )
 
 
     class Meta:
+        """
+        Meta class for AbstractItem
+        """
+
         abstract = True
 
     def __str__(self):
@@ -42,10 +58,23 @@ class AbstractChannels(models.Model):
         name = self.Channel_name
         return f'{name} '
 
-class UserChannel(AbstractChannels):
-    user = models.ForeignKey(User, unique=True)
 
-class CommonChannel(AbstractChannels):
+class UserChannel(AbstractChannel):
+    """
+    The model which defines channels particulaar to a single user
+    """
+    user = models.ForeignKey(
+        to=User, 
+        unique=True,
+        on_delete=models.CASCADE,
+    )
+
+
+class CommonChannel(AbstractChannel):
+    """
+    The model which defines common channels accessible to all channels.
+    """
+    pass
 
 
 
