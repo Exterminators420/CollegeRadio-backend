da266dc032821ba4e71ab112c6755e3383f84cba
torishabhmehta <torishabhmehta@gmail.com>
2018-09-30 03:32:15 +0530

    added chatsocket

--- backend/routing.py
+++ backend/routing.py
@@ -2,21 +2,20 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import player.routing
 import chatbox.routing
+from chatbox.consumers import ChatConsumer
+from player.consumers import PlayerConsumer
+from django.conf.urls import url
+
+
 
 application = ProtocolTypeRouter({
     'websocket' : AuthMiddlewareStack(
-        URLRouter(
-            player.routing.websocket_urlpatterns
-            
-        )
+        URLRouter([
+            url(r'^ws/chatbox/(?P<room_name>[^/]+)/$', ChatConsumer),
+            url(r'^ws/stream/(?P<room_name>[^/]+)/$', PlayerConsumer),   
+        ])
     ),
 })
 
-# application = ProtocolTypeRouter({
-#     'websocket' : AuthMiddlewareStack(
-#         URLRouter(
-#             
-#         )
-#     ),
-# })
+
 
--- chatbox/routing.py
+++ chatbox/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     url(r'^ws/chatbox/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    
 ]
